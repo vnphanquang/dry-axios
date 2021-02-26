395fb836154297857054aa69120456d2fc536e73
Quang Phan <contact.quangphan@gmail.com>
2021-02-26 22:01:35 +0700

    feat(util): export util Asserted

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './decorators/class';
 export * from './decorators/method';
 export * from './decorators/parameter';
+
+export function Asserted<T>(value?: any): T {
+  return value as T;
+}
